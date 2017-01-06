0727aa93f0a127f7a06f15b508c4f8c507d258ad
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-01-07 01:40:16 +0700

    r.R: add comments

--- 3/src/r.R
+++ 3/src/r.R
@@ -1,26 +1,35 @@
 
+# sample size
 n <- 100
 
+# generate sample from Cauchy distribution
 x <- rcauchy(n)
 
-# number of bins
+# number of intervals for grouping
 k <- 5
 
-x_i <- quantile(x, seq(from=0, to=1, by=1/k))
+# optimal probabilities vector step for AOG of sample from Cauchy distribution
+opt_P <- 1/k
 
-# if hypothesis is composite, then
-# estimate these parameters first
+# calculate intervals break points
+x_i <- quantile(x, seq(from=0, to=1, by=opt_P))
+
+# TODO: if hypothesis should be composite, then estimate these parameters first
 p <- pcauchy(x_i, location=0, scale=1)
 
+# calculate expected probabilities
 expected <- zoo::rollapply(p, width=2, function(x) x[2] - x[1])
 
 # group sample
 f <- cut(x, x_i, include.lowest=TRUE)
 groups <- split(x, f)
+
+# calculate observed probabilities
 counts <- lapply(groups, FUN=length)
 observed <- lapply(counts, function(x) x / n)
 observed <- as.numeric(observed)
 
+# perform test
 test <- chisq.test(observed, p=expected, rescale.p=TRUE)
 
 print(test$statistic)
