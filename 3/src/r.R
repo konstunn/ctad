baf448c0bb4999022f0943ad7e4add84c88a5ef6
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-01-22 22:52:41 +0700

    3/src/r.R: fix algorithm, add more comments

--- 3/src/r.R
+++ 3/src/r.R
@@ -13,16 +13,19 @@ k <- 5
 
 # optimal probabilities vector elements step for asymptotically optimal
 # grouping of sample from Cauchy distribution (we assume H0 is true)
-opt_P <- 1/k
+dP <- 1/k
+P <- seq(from=0, to=1, by=dP)
 
-# calculate intervals break points
-x_i <- quantile(x, seq(from=0, to=1, by=opt_P))
+# TODO: for testing composite hypothesis first estimate distribution parameters 
+# from x, assuming x is from Cauchy distribution (we assume H0 is true)
+location=0 # Here we test
+scale=1    # simple hypothesis
 
-# TODO: if hypothesis should be composite, then estimate these parameters first
-p <- pcauchy(x_i, location=0, scale=1)
+# calculate groups (bins) break points (i.e. quantiles)
+x_i <- qcauchy(P, location=location, scale=scale)
 
 # expected probabilities (frequencies)
-expected <- zoo::rollapply(p, width=2, function(x) x[2] - x[1])
+expected <- rep(dP, k)
 
 # perform sample grouping (binning)
 factors <- cut(x, x_i, include.lowest=TRUE)
@@ -33,7 +36,5 @@ counts <- lapply(groups, FUN=length)
 observed <- lapply(counts, function(x) x / n)
 observed <- as.numeric(observed)
 
-# perform goodness-of-fit test, calculate chi square test statistic
-test <- chisq.test(observed, p=expected, rescale.p=TRUE)
-
-print(test$statistic)
+# calculate chi square test statistic
+print(sum((observed-expected)^2 / expected))
