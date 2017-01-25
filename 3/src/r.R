37eee46c70cb7404d516366fe6162e7ca9abbc69
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-01-25 14:55:27 +0700

    Add messages and progress bars for some processes

--- 3/src/r.R
+++ 3/src/r.R
@@ -3,11 +3,14 @@
 N <- 2*10^4		# number of samples
 n <- 2*10^2		# sample size
 
+print("Random number samples generating...")
 # Generate sample
 # TODO: customize according to H
 # H0: sample is from Cauchy distribution
 # H1: sample is from Normal distribution
+print("Generating big sample...")
 X <- rcauchy(n*N) # here we set H0 true
+print("Splitting big sample into list of samples...")
 X <- split(X , ceiling(seq_along(X)/n))
 
 # number of intervals for grouping (number of bins)
@@ -30,15 +33,20 @@ x_i <- qcauchy(P, location=location, scale=scale)
 expected <- rep(dP, k)
 
 # perform sample grouping (binning)
-factors <- lapply(X, cut, x_i, include.lowest=TRUE)
+print("Cutting...")
+library(plyr)
+factors <- llply(X, cut, x_i, include.lowest=TRUE, .progress='text')
+print("Splitting...")
 groups <- Map(split, X, factors)
 
 # calculate observed probabilities (frequencies)
+print("Calculating observed probs...")
 nestlen <- function(x) lapply(x, length)
-observed <- lapply(groups, nestlen)
+observed <- llply(groups, nestlen, .progress='text')
 observed <- matrix(unlist(observed), ncol=k, byrow=TRUE)
 
 # calculate chisq test statistic values
+print("Calculating test statistic array...")
 machisq.test <- function(o, p) chisq.test(o, p=p)$statistic
 chisq <- apply(observed, 1, machisq.test, p=expected)
 
