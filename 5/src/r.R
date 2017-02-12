dd39b21f5effb4a1284e1c8c1fe7c321e987e232
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-02-12 21:04:13 +0700

    5: add R source

---
+++ 5/src/r.R
@@ -0,0 +1,94 @@
+#!/usr/bin/Rscript --vanilla
+
+# Kolmogorov test statistic
+# TODO: calc by ks.test
+# distributed as kolmim::pkolm
+Sk <- function(X, P) {
+	X <- sort(X)
+	n <- length(X)
+	i <- seq(1, n)
+	Dplus <- max(i/n - P(X, theta))
+	Dminus <- max(P(X, theta) - (i-1)/n)
+	Dn <- max(Dplus, Dminus)
+	Sk <- (6*n*Dn + 1)/(6 * sqrt(n))
+	return(Sk)
+}
+
+# Smirnov test statistic
+# distributed as chi-squared, df = 2
+Sm <- function(X, P) {
+	X <- sort(X)
+	n <- length(X)
+	i <- seq(1, n)
+	Dplus <- max(i/n - P(X, theta))
+	Sm <- (6*n*Dplus+1)^2/9*n
+	return (Sm)
+}
+
+# cvm.cdf
+# cvm.test
+
+# ad.cdf
+# ad.test
+
+chisq.statmod.gof2cauchy <- function(n=100, N=16600, Htype, trueH, k)
+{
+	sink(stderr())
+	message("")
+	message(deparse(sys.calls()[[sys.nframe()]]))
+	true_H <- paste('r', trueH, sep='')
+	message("Random number samples generating...")
+	message("Generating big sample...")
+	# Generate sample
+	X <- do.call(true_H, args=list(n=n*N))
+	message("Splitting big sample into list of samples...")
+	X <- split(X , ceiling(seq_along(X)/n))
+
+	# TODO: customize this according to H0 (now it's Cauchy)
+	# optimal probabilities vector elements step for asymptotically optimal
+	# grouping of sample from Cauchy distribution
+	dP <- 1/k
+	P <- seq(from=0, to=1, by=dP)
+
+	library(fitdistrplus)
+	library(plyr)
+
+	if (Htype == 'simple') {
+		message("Simple hypothesis.")
+		estimates <- c(rep(c(location=0,scale=1), N))
+		estimates <- split(estimates, ceiling(seq_along(estimates)/2))
+	} else if (Htype == 'complex') {
+		message("Complex hypothesis.")
+		message("Estimating parameters...")
+		lmledist <- function(x, d) mledist(x, d)$estimate
+		estimates <- llply(X, lmledist, 'cauchy', .progress='text')
+	} else stop("Invalid Htype value: must be 'simple' or 'complex'")
+
+	# TODO: customize this according to H0 (now it's Cauchy)
+	# calculate groups (bins) break points (i.e. quantiles)
+	lqcauchy <- function(e, p) qcauchy(p, e[1], e[2])
+	x_i <- llply(estimates, lqcauchy, p=P)
+
+	# expected probabilities (frequencies)
+	expected <- rep(dP, k)
+
+	# perform sample grouping (binning)
+	message("Cutting...")
+	factors <- Map(cut, X, x_i)
+	message("Splitting...")
+	groups <- Map(split, X, factors)
+
+	# calculate observed probabilities (frequencies)
+	message("Calculating observed probs...")
+	llen <- function(x) lapply(x, length)
+	observed <- llply(groups, llen, .progress='text')
+	observed <- matrix(unlist(observed), ncol=k, byrow=TRUE)
+
+	# calculate chisq test statistic values
+	message("Calculating test statistic array...")
+	lchisq.test <- function(o, p) chisq.test(o, p=p)$statistic
+	chisq <- apply(observed, 1, lchisq.test, p=expected)
+	sink()
+	return(data.frame(x=chisq, k=rep(k,N), n=rep(n,N), N=rep(N,N),
+					  trueH=rep(trueH,N), Htype=rep(Htype,N)))
+}
