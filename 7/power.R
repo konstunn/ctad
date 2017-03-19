30fddb28b481dbdec4d2d78e10560e8d3c5fb54e
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-03-19 08:47:57 +0700

    Add power function in a separate source file

---
+++ 7/power.R
@@ -0,0 +1,19 @@
+power <- function(dfin, alpha)
+{
+	# H0
+	dfnull <- subset(dfin, H=='norm')
+
+	# H1
+	dfalt <- subset(dfin, H=='pgnorm')
+
+	# empirical quantiles
+	q <- quantile(dfnull$x, probs=1-alpha)
+
+	F1 <- ecdf(dfalt$x)
+	# F1 is function
+	percentile <- F1(q)
+
+	df <- data.frame(alpha=alpha, power=1-percentile)
+	return(df)
+}
+
